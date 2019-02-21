7ad9c1d20896dc2e8fc9f7b059d828b442089b92
sanjaykakkar222 <47442582+sanjaykakkar222@users.noreply.github.com>
2019-02-21 13:43:26 +0530

    Add files via upload

---
+++ Clock assignment/external.js
@@ -0,0 +1,50 @@
+function displayday()
+{
+
+
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    var n = weekday[d.getDay()];
+
+    document.getElementById("day").innerHTML=n;
+
+
+
+}
+
+function displaydate()
+{
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    
+    newdate = year + "/" + month + "/" + day;
+
+    document.getElementById("date").innerHTML=newdate;
+}
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
